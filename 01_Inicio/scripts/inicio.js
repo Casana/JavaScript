b69762607923b7e37d31fa10e1282c37675d8d44
Casana <carlos.martinez.casana@tajamar365.com>
2017-05-17 21:39:16 +0200

    Final clase miercoles

---
+++ 01_Inicio/scripts/inicio.js
@@ -0,0 +1,31 @@
+/**
+ * Variables
+ */
+
+const SALUDO = "Hola ";
+
+// Acceso al Node btn1 del DOM
+var oBtn1 = document.querySelector('#btn1');
+// Acceso al Node nombre del DOM
+var oNombre = document.querySelector('#nombre');
+//Acceso al Node respuesta del DOM
+var oRespuesta = document.querySelector('#respuesta');
+
+/**
+ * Funciones
+ */
+
+function saludar () {
+    let sSaludo = SALUDO;
+    sSaludo += oNombre.value;
+    oRespuesta.innerHTML = "<b>" + sSaludo + "</b>" ;
+}
+
+
+
+oBtn1.onclick = saludar;
+
+console.dir(oBtn1);
+console.dir(oNombre);
+console.dir(oRespuesta);
+

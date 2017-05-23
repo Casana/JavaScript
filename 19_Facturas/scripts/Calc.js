32466af58dc6f90b73ec20f419a135a3dce2e97b
Casana <carlos.martinez.casana@tajamar365.com>
2017-05-23 18:43:29 +0200

    clase martes 23

--- 19_Facturas/scripts/Calc.js
+++ 19_Facturas/scripts/Calc.js
@@ -1,33 +1,30 @@
 // Crear un único objeto que calcule importe de factura (por consola)
 
- let Factura = {
+let Factura = {
 
-    empresa: {
-        nombre: "My Journey SA",
-        direccion: "C/ Gran via 10, 2ºC",
-        teléfono: "957 444 333",
-        CIF: "X-55443311",
-    },
+        empresa: {
+            nombre: "My Journey SA",
+            direccion: "C/ Gran via 10, 2ºC",
+            teléfono: "957 444 333",
+            CIF: "X-55443311",
+        },
 
-    cliente: {
-        nombre:"Fundacion Tajamar",
-        direccion:"c/Pio Felipe 12",
-        teléfono:"918 56 54 51",
-        CIF:"",
-    },
+        cliente: {
+            nombre: "Fundacion Tajamar",
+            direccion: "c/Pio Felipe 12",
+            teléfono: "918 56 54 51",
+            CIF: "",
+        },
 
-    productos: [
-        {descripcion: "Banners", cantidad: 5, precio: 90},
-        {descripcion: "SuperBanners", cantidad: 3, precio: 150},
-        {descripcion: "RobaPáginas", cantidad: 8, precio: 120},
-        {descripcion: "Rascacielos", cantidad: 4, precio: 110}
-    ],
+        productos: [
+            { descripcion: "Banners", cantidad: 5, precio: 90 },
+            { descripcion: "SuperBanners", cantidad: 3, precio: 150 },
+            { descripcion: "RobaPáginas", cantidad: 8, precio: 120 },
+            { descripcion: "Rascacielos", cantidad: 4, precio: 110 }
+        ],
 
-    informacion: {
-        importetotal: {value:""},
-        tipodeIVA: {value:""},
-        formadepago: {value:""},
-    }
-
-
-
+        informacion: {
+            importetotal: { value: "" },
+            tipodeIVA: { value: "" },
+            formadepago: { value: "" },
+        }
--- Serie_Fibonacci/scripts/inicio.js
+++ Serie_Fibonacci/scripts/inicio.js
@@ -1,14 +1,13 @@
 //Serie Fibonacci: Empenzando por 0 y 1, cada término es la suma de los dos números anteriores de la serie.
 
 var i;
-var fib= [];
-iterar=prompt ("¿Cuantas iteraciones quieres?");
+var fib = [];
+iterar = prompt("¿Cuantas iteraciones quieres?");
 fib[0] = 0;
 fib[1] = 1;
 document.write(fib[0] + " ");
 document.write(fib[1] + " ");
-for(i=2; i<=iterar; i++)
-{
-    fib[i] = fib[i-2] + fib[i-1];
-    document.write(fib [i] + " ")
+for (i = 2; i <= iterar; i++) {
+    fib[i] = fib[i - 2] + fib[i - 1];
+    document.write(fib[i] + " ")
 }
